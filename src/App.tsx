f5cebc3adb383ca98f218b2f91c94078eb4a139f
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-27 00:28:31 +0330

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-function App() {
+const App = () => {
 
   return (
-      <div>
-
+      <div className="flex flex-col items-center justify-center gap-16 py-32">
+        <button className="px-4 py-2 bg-red-500 text-white">Button</button>
       </div>
   )
 }
