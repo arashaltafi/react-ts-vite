ef7ecad41088d0eeee01c7c2d8b7e3134a847cd1
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-27 00:34:07 +0330

    a

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-12700680'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5ola8o0rmp8"
+    "revision": "0.p0htqae3fd"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 const App = () => {
 
   return (
-      <div className="flex flex-col items-center justify-center gap-16 py-32">
-        <button className="px-4 py-2 bg-red-500 text-white">Button</button>
-      </div>
+    <div className="flex flex-col items-center justify-center gap-16 py-32">
+      <button className="px-4 py-3 text-xl rounded-lg bg-red-500 text-center text-white">Button</button>
+    </div>
   )
 }
 
