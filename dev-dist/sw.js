c7f31bfc2e230ca7abcaea9fc46385877c666e06
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-27 00:32:49 +0330

    a

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-12700680'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.k1rdmhsm5hg"
+    "revision": "0.5ola8o0rmp8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
