8d38d1ce453157544dc675f77e7bf4138abe2f54
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-27 00:41:45 +0330

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 const App = () => {
 
   return (
-    <div className="flex flex-col items-center justify-center gap-16 py-32">
-      <button className="px-4 py-3 text-xl rounded-lg bg-red-500 text-center text-white">Button</button>
+    <div className="flex flex-col items-center justify-center gap-16 py-16 px-8">
+      <button className="px-4 py-3 text-xl rounded-lg bg-red-500 text-center text-white cursor-pointer">
+        Button
+      </button>
+
+      <img
+        className="size-56 rounded-xl shadow-xl shadow-black cursor-pointer"
+        src="https://arashaltafi.ir/arash.jpg"
+        alt="arash altafi" />
+
+      <p className="text-xl text-center w-[80%] text-black cursor-text">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Quod qui earum quibusdam? Facilis tempora quia nesciunt rerum expedita culpa officiis incidunt,
+        molestiae natus dolor quaerat pariatur fuga veniam commodi vero.
+      </p>
     </div>
   )
 }
